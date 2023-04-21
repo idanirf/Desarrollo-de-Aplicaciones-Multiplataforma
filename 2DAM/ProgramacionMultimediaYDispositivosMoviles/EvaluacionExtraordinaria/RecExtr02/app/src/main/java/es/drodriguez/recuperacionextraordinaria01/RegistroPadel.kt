f6783df76a0300c97ee3eab95f6f2269e3704c06
Daniel Rodriguez Fernandez <danielrodrifern@gmail.com>
2023-04-21 13:16:18 +0200

    PMyDM 02

---
+++ 2DAM/ProgramacionMultimediaYDispositivosMoviles/EvaluacionExtraordinaria/RecExtr02/app/src/main/java/es/drodriguez/recuperacionextraordinaria01/RegistroPadel.kt
@@ -0,0 +1,11 @@
+package es.drodriguez.recuperacionextraordinaria01
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class RegistroPadel : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_registro_padel)
+    }
+}
